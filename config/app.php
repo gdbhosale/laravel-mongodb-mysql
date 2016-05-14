5f31e10ccb07ffea800cd1e421c9d2aec4f24212
Ganesh Bhosale <gdb.sci123@gmail.com>
2016-05-14 14:27:01 +0530

    srtaing with gen. model Book

--- config/app.php
+++ config/app.php
@@ -147,7 +147,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        Jenssegers\Mongodb\MongodbServiceProvider::class,
         /*
          * Application Service Providers...
          */
